5f36be6f4d2506592c5b668ca2930ef36ed9532b
Germain Lecorps <germain.lecorps-ext@soitec.com>
2021-11-29 10:58:35 +0100

    first commit

---
+++ days/day01.ts
@@ -0,0 +1,5 @@
+export class Day01 {
+  public displayDay() {
+    return 'Day01';
+  }
+}
---
+++ main.ts
@@ -0,0 +1,5 @@
+import { Day01 } from './days/day01';
+
+const day01 = new Day01();
+
+console.log(day01.displayDay());
