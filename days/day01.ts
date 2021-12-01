8df42cc1a03c532cfef61ed7c4ad91d8eacabad0
DESKTOP-IQMPPVD\Administrateur <roulade.glissade@gmail.com>
2021-12-01 16:14:09 +0100

    Day01 working

--- days/day01.ts
+++ days/day01.ts
@@ -1,5 +1,18 @@
 export class Day01 {
-  public displayDay() {
-    return 'Day01';
+  input_data: string[];
+  constructor(input: string[]) {
+    this.input_data = input;
+  }
+
+  public part1(): string {
+    return this.input_data.filter((d, i) => this.input_data[i - 1] && parseInt(d) > parseInt(this.input_data[i - 1])).length.toString();
+  }
+
+  public part2(): string {
+    return this.input_data
+      .filter((value, idx) => value && this.input_data[idx + 1] && this.input_data[idx + 2])
+      .map((value, idx) => parseInt(value) + parseInt(this.input_data[idx + 1]) + parseInt(this.input_data[idx + 2]))
+      .filter((value, idx, current) => this.input_data[idx - 1] && value > current[idx - 1])
+      .length.toString();
   }
 }
--- main.ts
+++ main.ts
@@ -1,5 +1,8 @@
 import { Day01 } from './days/day01';
+import * as fs from 'fs';
 
-const day01 = new Day01();
+const file = fs.readFileSync('inputs/input_day01.txt', 'utf8').split('\n');
 
-console.log(day01.displayDay());
+const day01 = new Day01(file);
+
+console.log(day01.part2());
